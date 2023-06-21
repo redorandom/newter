2adca918a4e94fd4a1a915c62c8b62dfc6fd4870
redorandom <rkdxoalsking@naver.com>
2023-06-21 11:18:49 +0900

    오랫만에 커밋함

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCB60_yGaeBJCm-LX4EztIYfstdLrjNSlg",
+    authDomain: "newter-bb350.firebaseapp.com",
+    projectId: "newter-bb350",
+    storageBucket: "newter-bb350.appspot.com",
+    messagingSenderId: "750687147326",
+    appId: "1:750687147326:web:8a5e7219d316b2a6ed486f"
+};
+
+export default firebase.FirebaseErrorinitializeApp(firebaseConfig);
