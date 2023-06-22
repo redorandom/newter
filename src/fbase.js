2433e7e6acc09c7dd8bf1fb62ebedf2e5142fb96
redorandom <rkdxoalsking@naver.com>
2023-06-22 11:19:41 +0900

    0622

--- src/fbase.js
+++ src/fbase.js
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-    appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
@@ -14,3 +16,5 @@ const firebase = initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = getAuth();
+
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createdAt: serverTimestamp(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        ></input>
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
-
